e6c551550bfe4b805a523e1dcfc05eea1be45a7d
hal <wyyxleihao@163.com>
2024-01-16 10:36:08 +0800

    改为只过滤 armeabi-v7a，减小包名 - hal

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,20 +6,16 @@ plugins {
 android {
     namespace = "com.hal.leitt"
     compileSdk = 33
-
     defaultConfig {
         applicationId = "com.hal.leitt"
         minSdk = 24
         targetSdk = 33
         versionCode = 1
         versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         ndk {
             abiFilters.add("armeabi-v7a")
         }
     }
-
     buildTypes {
         release {
             isMinifyEnabled = true
@@ -51,7 +47,4 @@ dependencies {
     implementation("com.google.android.material:material:1.8.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.preference:preference:1.2.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
